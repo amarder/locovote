d3137d61146bae0e35bbe924bd6f12578a968974
Andrew Marder <andrew.marder@caveon.com>
2025-06-06 11:59:38 -0400

    Update data loader

--- frontend/src/data/general-fund.arrow.sh
+++ frontend/src/data/general-fund.arrow.sh
@@ -1,1 +1,2 @@
-cat ../data/processed/combined_general_fund.arrow
+# NOTE: I manually uploaded this file to the release because it was taking too long to build with github actions
+curl -L -f https://github.com/amarder/locovote/releases/download/v0.1.1/combined_general_fund.arrow
